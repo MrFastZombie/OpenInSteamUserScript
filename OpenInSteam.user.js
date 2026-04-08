79fc7cd43ad648769c3e07ee11f271a5c31690e1
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2026-04-08 00:17:44 -0500

    Version 0.6 - Quickview update

--- OpenInSteam.user.js
+++ OpenInSteam.user.js
@@ -1,26 +1,49 @@
 // ==UserScript==
 // @name         OpenInSteam
-// @version      0.5
+// @version      0.6
 // @description  Adds a button next to the installation button to open the current Steam page in the client.
 // @author       MrFastZombie
 // @match        https://*.steampowered.com/*
 // @match        https://steamcommunity.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_addStyle
 // @run-at document-end
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+    //---------------------------------------- CONFIG ----------------------------------------
+    // If you wish to configure these settings, use the configuration command for this userscript in your userscript manager.
+    var cfg = new MonkeyConfig({
+      title: 'OpenInSteam Configuration',
+      menuCommand: true,
+      params: {
+        initial_Delay: {
+          type: 'number',
+          default: 1000
+        },
+        quickview_Delay: {
+          type: 'number',
+          default: 250
+        },
+        inject_quickview: {
+          type: 'checkbox',
+          default: true
+        }
+      }
+    });
 
-    var initialDelay = 750; //Delay in milliseconds before attempting to inject the button. 1 second = 1000 ms. Increase this if you notice the button disappearing and reappearing when opening a page!
-
+    var initialDelay = cfg.get('initial_Delay'); //Delay in milliseconds before attempting to inject the button. 1 second = 1000 ms. Increase this if you notice the button disappearing and reappearing when opening a page!
+    var quickviewDelay = cfg.get('quickview_Delay'); //Delay before injecting quickview button. Tends to not have issues with quicker times.
+    var injectQuickViewButton = cfg.get('inject_quickview'); //Will add a quick view button if true.
+  //---------------------------------------- END OF CONFIG ----------------------------------------
+  //---------------------------------------- NEW INJECTION ----------------------------------------
     function attemptNewInject(delay, pdiv, indom, iurl) {
-      var nbutton = indom.query('<div>', {
-          id: 'openClient'
-      });
-
       var nbuttoncontent = indom.query('<a>', {
           class:"openClientContent",
           href: "steam://openurl/" + iurl,
@@ -36,7 +59,6 @@
         newDiv.onmouseenter = function() { //Change the href when the user is about to press the button.
           document.querySelector("#OpenInSteamButtonDiv").shadowRoot.querySelector("a").href = "steam://openurl/" + window.location.href;
         };
-        //newDiv.css({"display":"inline-block","height":"21px","line-height":"24px","margin-right":"3px"});
         var shadow = newDiv.attachShadow({ mode: "open" });
         pdiv.prepend(newDiv);
         shadow.appendChild(nbuttoncontent[0]);
@@ -52,6 +74,7 @@
         return;
       }, delay)
     }
+  //---------------------------------------- END OF NEW INJECTION ----------------------------------------
 
     var dom = {};
     dom.query = jQuery.noConflict( true );
@@ -78,10 +101,37 @@
     if(inbutton[0] == undefined) { //If the inbutton is undefined, we are on the new workshop layout
       attemptNewInject(initialDelay, parentDiv, dom, pageurl);
 
-    } else {
+    } else { //Normal page injection
       newbutton.prependTo("#global_action_menu");
       newbuttonContent.appendTo(newbutton);
     }
 
-
+    // ---------------------------------------- Quick View Injection ----------------------------------------
+    document.body.addEventListener('click', function(e) { //Potentially run when a click is detected.
+      var checkDiv = document.querySelector("#OpenInSteamButtonDivModView");
+      if(inbutton[0] == undefined && checkDiv == null && injectQuickViewButton == true) { //Do not run if on a non-beta page, the button already exists, or if quickview button is disabled.
+        setTimeout(function() {
+          var modHeader = document.querySelector('h2:has(a)'); //Find the mod header by searching for an h2 that has a <a> in it.
+          if(modHeader != null) { //Only run if Quickview is open.
+            var modA = document.querySelector('h2:has(a) > a'); //Find the mod header, then select its <a>.
+            var subButton = dom.query("button:contains(Subscribe)")[0]; //Select the subscribe button
+            var buttonDiv = subButton.parentNode.parentNode.parentNode; //Go three nodes up to get the proper div to inject.
+            var nDiv = document.createElement("div");
+            nDiv.setAttribute('style', 'display: inline-block;');
+            nDiv.setAttribute("id", "OpenInSteamButtonDivModView");
+            var nbutton2 = subButton.cloneNode(true); //Clone the sub button to be turned into the new button.
+            nbutton2.textContent = ""; //Remove its text
+            nbutton2.setAttribute('style', '--min-width: fit-content; padding: 0px; max-height: stretch')
+            var linka = document.createElement("a");
+            linka.href = "steam://openurl/" + modA.href;
+            linka.text = "Open in client";
+            linka.setAttribute('style', 'width: auto; height: auto; text-decoration: none; padding-right: 7px; padding-left: 29px; padding-top: 7px; height: stretch; text-wrap-mode: nowrap; background-image: url(https://raw.githubusercontent.com/MrFastZombie/OpenInSteamUserScript/main/OpenInSteamIcon.png); background-repeat: no-repeat; background-position-y: center; background-position-x: 8px');
+            buttonDiv.prepend(nDiv);
+            nbutton2.prepend(linka);
+            nDiv.prepend(nbutton2);
+          }
+        }, 250)
+      }
+    });
+    //---------------------------------------- Quick View Injection end ----------------------------------------
 })();
